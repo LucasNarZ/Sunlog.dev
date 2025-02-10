265ade97935661424e6aea9c4ec5222c28704d1e
LucasNarZ <LucasNarloch123@gmail.com>
2025-02-10 10:45:17 -0300

    adding Post to  app module

--- BackEnd/src/app.module.ts
+++ BackEnd/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { PostsModule } from './Post/posts.module';
+import { PostsController } from './Post/posts.controller';
+import { PostsService } from './Post/posts.service';
+import { postsProviders } from './Post/posts.providers';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    DatabaseModule,
+    PostsModule
+  ],
+  controllers: [
+    AppController,
+    PostsController
+  ],
+  providers: [
+    AppService,
+    PostsService,
+    ...postsProviders
+  ],
 })
 export class AppModule {}
