ac8d8f635ae855e92bd206c0215115bd148f4cf2
LucasNarZ <lucasnarloch123@gmail.com>
2025-02-11 17:06:22 -0300

    adding validation decorators to post dto class

--- BackEnd/src/dtos/post.dto.ts
+++ BackEnd/src/dtos/post.dto.ts
@@ -1,7 +1,21 @@
+import { IsArray, IsNotEmpty, IsString } from "class-validator"
+
 export class createPostDto {
+    @IsString()
+    @IsNotEmpty()
     readonly title:string;
+    @IsString()
+    @IsNotEmpty()
     readonly content: string;
+    @IsString()
+    @IsNotEmpty()
     readonly author:string;
+    @IsArray()
+    @IsNotEmpty()
+    @IsString({each:true})
     readonly categorys?:string[];
+    @IsArray()
+    @IsNotEmpty()
+    @IsString({each:true})
     readonly tags?:string[];
 }
