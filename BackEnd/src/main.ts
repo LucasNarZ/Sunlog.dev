687d2067d2e8b7a8104fad1a08705a3579692e36
LucasNarZ <lucasnarloch123@gmail.com>
2025-02-17 13:05:07 -0300

    adding global filter to app

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalFilter } from './filters/ValidationErroItem.filter';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet())
   app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalFilters(new GlobalFilter())
   await app.listen(process.env.PORT ?? 3000);
 }
 
