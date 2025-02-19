4b78250c9fa5943f0c7751254724d72cae439c6b
LucasNarZ <lucasnarloch123@gmail.com>
2025-02-19 13:10:05 -0300

    changing GlobalFilter import

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { GlobalFilter } from './filters/ValidationErroItem.filter';
+import { GlobalFilter } from './filters/globalFilter.filter';
 import helmet from 'helmet';
 
 async function bootstrap() {
