e9ed21532dfbc3b2541e87b35582d709c4f76c3d
LucasNarZ <lucasnarloch123@gmail.com>
2025-02-19 13:10:24 -0300

    catching unique constraint exception

--- BackEnd/src/User/users.controller.ts
+++ BackEnd/src/User/users.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Delete } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { createUserDto } from "src/dtos/user.dto";
 import * as argon2 from "argon2"
+import { UniqueConstraintException } from "src/exceptions/uniqueContraint.exception";
 
 @Controller()
 export class UsersController {
@@ -16,12 +17,21 @@ export class UsersController {
 
     @Post("user")
     async createUser(@Body() body:createUserDto) {
-        let data = body
-        data = {
-            ...data,
-            password: await argon2.hash(data.password)
+        try{
+            let data = body
+            data = {
+                ...data,
+                password: await argon2.hash(data.password)
+            }
+            return await this.usersService.createUser(data)
+        }catch(err){
+            
+            if(err.name == 'SequelizeUniqueConstraintError'){
+                console.log(err)
+                throw new UniqueConstraintException(err.errors[0].message)
+            }
         }
-        return await this.usersService.createUser(data)
+
 
     }
 
