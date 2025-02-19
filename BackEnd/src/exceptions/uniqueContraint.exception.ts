31b52ab198cf1d31ff603be4cc8ca61cf1d55f16
LucasNarZ <lucasnarloch123@gmail.com>
2025-02-19 13:09:39 -0300

    creating unique constraint exception

---
+++ BackEnd/src/exceptions/uniqueContraint.exception.ts
@@ -0,0 +1,9 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
+
+export class UniqueConstraintException extends HttpException {
+    constructor (
+        readonly message:string
+    ) {
+        super(message, HttpStatus.BAD_REQUEST)
+    }
+}
