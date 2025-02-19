03dd326feb2a97c2e2e6cc7d5bd167bc12f81016
LucasNarZ <lucasnarloch123@gmail.com>
2025-02-19 13:08:45 -0300

    creating global filter

---
+++ BackEnd/src/filters/globalFilter.filter.ts
@@ -0,0 +1,23 @@
+import { ArgumentsHost, BadRequestException, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { ValidationError } from "sequelize";
+import { Response, Request } from "express";
+
+@Catch(HttpException)
+export class GlobalFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const message = exception.message
+
+    response
+      .status(status)
+      .json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        message
+      });
+  }
+}
