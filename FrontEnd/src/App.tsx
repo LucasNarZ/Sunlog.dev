bee663a4d0beebed66b97c672df5b00d9e267c9e
LucasNarZ <lucasnarloch123@gmail.com>
2025-05-08 13:28:45 -0300

    adding sign routes

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Post from './pages/post'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/signUp'
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:postId" element={<Post />} />
+        <Route path='/SignIn' element={<SignIn />}/>
+        <Route path='/SignUp' element={<SignUp />}/>
+
       </Routes>
     </BrowserRouter>
   )
