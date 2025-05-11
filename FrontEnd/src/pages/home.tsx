0eae917953fa6166e396440fd62be6ee1d9c117e
LucasNarZ <LucasNarloch123@gmail.com>
2025-05-11 17:54:49 -0300

    showing posts by tag on home page

--- FrontEnd/src/pages/home.tsx
+++ FrontEnd/src/pages/home.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import Header from '../components/header'
+import CardPost from '../components/cardPost'
+import { usePostsByTag } from '../hooks/getFilteredPosts'
+import { useSearchParams } from 'react-router-dom'
+import type { Post } from '../types/post'
 
 const Home = () => {
+  const [ searhParams, setSearchParams ] = useSearchParams()
+  const [ posts, errorPosts ] = usePostsByTag(searhParams.getAll("tag"))
+  
+  if(errorPosts){
+    console.error(errorPosts)
+  }
+
   return (
     <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+      <Header />
+      <div className='w-full flex flex-col items-center pt-12'>
+          {posts ? posts.map((post:Post, index:number) => {
+            return (
+              <CardPost key={index} post={post} />
+            )
+          }): <div>Loading...</div>}
+      </div>
     </div>
   )
 }
