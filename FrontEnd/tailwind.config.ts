7403d1080791f4680644af95f33a488a1feae8d2
LucasNarZ <lucasnarloch123@gmail.com>
2025-05-14 16:51:58 -0300

    creating tailwind config file

---
+++ FrontEnd/tailwind.config.ts
@@ -0,0 +1,12 @@
+
+export default {
+    theme: {
+      extend: {
+        fontFamily: {
+          Garamont: ['"EB Garamond"', "serif"],
+          // Add more custom font families as needed
+        },
+      },
+    },
+    // Other Tailwind configuration settings
+  };
