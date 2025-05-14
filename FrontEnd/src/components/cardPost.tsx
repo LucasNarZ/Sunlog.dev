74ba5c4a59e7e619ed7005479ef78cf52bf2caf0
LucasNarZ <lucasnarloch123@gmail.com>
2025-05-14 16:53:14 -0300

    improving box layout

--- FrontEnd/src/components/cardPost.tsx
+++ FrontEnd/src/components/cardPost.tsx
@@ -12,10 +12,21 @@ const CardPost = ({ post }:CardPostProps) => {
         console.error(error)
     }
     return (
-        <div className="w-10/12 max-w-96 flex flex-col">
-            <div>{post.title}</div>
-            <div>{author?.name}</div>
-            <div>{creationData.getDay() + 1} / {creationData.getMonth() + 1}</div>
+        <div className="w-10/12 max-w-[510px] flex justify-between border-1 p-6 rounded-3xl border-gray-100">
+            <div className="flex flex-col">
+                <div className="flex items-center gap-5">
+                    <img className="w-7 aspect-auto" src={author?.profileImgUrl} alt="profilePic" />
+                    <p className="text-md">{author?.name}</p>
+                    <p className="text-sm opacity-60">{creationData.getDate()} / {creationData.getMonth() + 1}</p>
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-xl">{post.title}</p>
+                    <p className="text-xs opacity-85">{post.description}</p>
+                </div>
+            </div>
+
+            <img className="w-24 aspect-auto mr-5" src={post.previewImgUrl} alt="" />
+
         </div>
     )
 }
