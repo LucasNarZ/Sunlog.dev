a039d022b9cffa73a5c56e06b07995f364da39ca
LucasNarZ <lucasnarloch123@gmail.com>
2025-05-20 16:39:34 -0300

    creating sign pages

--- FrontEnd/src/pages/signUp.tsx
+++ FrontEnd/src/pages/signUp.tsx
@@ -1,10 +1,50 @@
-import React from "react"
+import { useState } from "react"
+import { apiClient } from "../apiClient"
+import { useNavigate } from "react-router-dom"
 
 const SignUp = () => {
+    const [ name, setName ] = useState("")
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
+
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e:any) => {
+        e.preventDefault();
+        try{
+            const body = {
+                name,
+                email,
+                password
+            }
+            const response = await apiClient.post("/user/register", body)
+            if(response.status == 201){
+                navigate("/profile")
+            }
+        }catch(err){
+            console.error(err)
+        }
+    }
+
     return (
-        <React.Fragment>
-            signUp
-        </React.Fragment>
+        <form className="h-[100vh] w-[100vw] flex justify-center items-center bg-gray-200" onSubmit={handleSubmit}>
+            <div className="flex flex-col bg-white w-11/12 max-w-96 h-[600px] rounded p-10 gap-y-7">
+                <p className="self-center text-2xl">SignUp</p>
+                <div className="flex flex-col">
+                    <label>Name</label>
+                    <input type="text" className="border-1 rounded" value={name} onChange={(e) => setName(e.target.value)}/>
+                </div>
+                <div className="flex flex-col">
+                    <label>Email</label>
+                    <input type="text" className="border-1 rounded" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className="flex flex-col">
+                    <label>Password</label>
+                    <input type="text" className="border-1 rounded" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <input type="submit" value="Sign Up" className="cursor-pointer border-1 w-34 self-center mt-auto"/>
+            </div>
+        </form>
     )
 }
 
