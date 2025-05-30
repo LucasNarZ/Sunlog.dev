29b3e3a6131966655e22e56f0e9f8244f6de8432
LucasNarZ <lucasnarloch123@gmail.com>
2025-05-30 13:58:18 -0300

    getting data to profile

--- FrontEnd/src/pages/profile.tsx
+++ FrontEnd/src/pages/profile.tsx
@@ -1,9 +1,21 @@
-
+import { redirect, useNavigate } from "react-router-dom"
+import Header from "../components/header"
+import useAuthor from "../hooks/getAuthor"
+import useGetProfile from "../hooks/getProfile"
 
 const Profile = () => {
+    const [ userData, errorProfile ] = useGetProfile()
+    const [ user, errorUser ] = useAuthor(userData?.userId ?? "")
+    const navigate = useNavigate()
+
+    if(errorProfile){
+        navigate("/signIn")
+    }
+
     return (
         <div>
-            profile
+            <Header />
+            {user?.name}
         </div>
     )
 }
