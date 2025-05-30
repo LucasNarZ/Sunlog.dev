a17589532b1bef4bf75b25a388d3313dc7b7a0b2
LucasNarZ <lucasnarloch123@gmail.com>
2025-05-30 13:53:17 -0300

    exporting usersService to use in auth

--- BackEnd/src/User/users.module.ts
+++ BackEnd/src/User/users.module.ts
@@ -4,15 +4,14 @@ import { UsersService } from "./users.service";
 import { usersProviders } from "./users.providers";
 import { UsersController } from "./users.controller";
 
-
-
 @Module({
     imports: [DatabaseModule],
     controllers: [UsersController],
     providers: [
         UsersService,
         ...usersProviders
-    ]
+    ],
+    exports: [UsersService]
 })
 
 export class UsersModule {}
