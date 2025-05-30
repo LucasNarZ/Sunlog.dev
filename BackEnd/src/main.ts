26ac5d94c2df8f053df16ec4b075f2429f624949
LucasNarZ <lucasnarloch123@gmail.com>
2025-05-30 13:54:27 -0300

    removing session auth and adding cookies support

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -3,45 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalFilter } from './filters/globalFilter.filter';
 import helmet from 'helmet';
-import * as session from "express-session"
-import Redis from 'ioredis';
-import { RedisStore } from 'connect-redis';
-
-declare module 'express-session' {
-  export interface SessionData {
-    user: {email:string};
-  }
-}
-
-
-const redisSessionClient = new Redis({
-  host:"redis-sessions"
-})
-
-redisSessionClient.connect().catch(console.error)
-const redisStore = new RedisStore({
-  client: redisSessionClient,
-  prefix: "myapp:"
-})
-
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
   app.use(helmet())
-  app.use(
-    session({
-      store: redisStore,
-      secret: process.env.SESSION_SECRET as string,
-      resave: false, 
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true, 
-        secure: false, 
-        maxAge: 1000 * 60 * 60, 
-      },
-    })
-  )
+  app.use(cookieParser())
+  app.enableCors({
+    origin: "http://localhost",
+    credentials: true
+  })
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new GlobalFilter())
   await app.listen(process.env.PORT ?? 3000);
