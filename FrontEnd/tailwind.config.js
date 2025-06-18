88dfff85cfbc20338eed46707ec65af65b7c3704
LucasNarZ <lucasnarloch123@gmail.com>
2025-06-18 13:55:57 -0300

    add colors pattern

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
         fontFamily: {
           Garamond: ["EB Garamond", "serif"],
         },
+        colors: {
+          primary: "#0C3DFF",    
+          secondary: "#F59E0B",  
+          danger: "#DC2626",     
+          muted: "#6B7280",      
+          background: "#F2F2F2",   
+      },
       },
     },
   };
