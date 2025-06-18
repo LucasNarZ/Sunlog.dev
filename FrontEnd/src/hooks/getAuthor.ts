93705ddf12a42f05a5351a6970a789dacc220d42
LucasNarZ <lucasnarloch123@gmail.com>
2025-06-18 13:57:29 -0300

    use cookie to get author

--- FrontEnd/src/hooks/getAuthor.ts
+++ FrontEnd/src/hooks/getAuthor.ts
@@ -2,22 +2,22 @@ import { useEffect, useState } from "react"
 import { apiClient } from "../apiClient"
 import type { User } from "../types/user"
 
-const useAuthor = (authorId: string):[User | null, unknown] => {
+const useAuthor = ():[User | null, unknown] => {
     const [ error, setError ] = useState<unknown>(null);
     const [ response, setResponse ] = useState<User | null>(null);
     useEffect(() => {
-        if(authorId){
-            (async () => {
-                try{
-                    const response = await apiClient.get(`/user/${authorId}`)
-                    setResponse(response.data)
-                }catch(err){
-                    console.log(err)
-                    setError(err)
-                }
-            })()
-        }
-    }, [authorId])
+        (async () => {
+            try{
+                const response = await apiClient.get(`/user/id`,{
+                    withCredentials: true
+                })
+                setResponse(response.data)
+            }catch(err){
+                console.log(err)
+                setError(err)
+            }
+        })()
+    }, [])
 
     return [ response, error ]
 }
