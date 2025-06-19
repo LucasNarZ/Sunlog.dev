7441d115eb5a5017610eaea9dee5e93f74fc9ec6
LucasNarZ <LucasNarloch123@gmail.com>
2025-06-19 16:42:16 -0300

    change for finding by slug

--- FrontEnd/src/hooks/getPost.ts
+++ FrontEnd/src/hooks/getPost.ts
@@ -2,21 +2,21 @@ import { useEffect, useState } from "react"
 import { apiClient } from "../apiClient"
 import type { Post } from "../types/post"
 
-const usePost = (postId: string):[Post | null, unknown] => {
+const usePost = (slug: string):[Post | null, unknown] => {
     const [ error, setError ] = useState<unknown>(null);
     const [ response, setResponse ] = useState<Post | null>(null);
     useEffect(() => {
         (async () => {
             try{
-                const response = await apiClient.get(`/post/${postId}`)
-                setResponse(response.data)
+                const response = await apiClient.get(`/post/${slug}`);
+                setResponse(response.data);
             }catch(err){
-                setError(err)
+                setError(err);
             }
         })()
-    }, [postId])
+    }, [slug])
 
-    return [ response, error ]
+    return [ response, error ];
 }
 
-export default usePost
+export default usePost;
