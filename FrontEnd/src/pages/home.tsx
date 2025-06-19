50fe43cc4e6adfde6da0c955e013e938112666ec
LucasNarZ <LucasNarloch123@gmail.com>
2025-06-19 16:44:44 -0300

    using new font theme style

--- FrontEnd/src/pages/home.tsx
+++ FrontEnd/src/pages/home.tsx
@@ -5,7 +5,7 @@ import { useSearchParams } from 'react-router-dom'
 import type { Post } from '../types/post'
 
 const Home = () => {
-  const [ searhParams, setSearchParams ] = useSearchParams()
+  const [ searhParams ] = useSearchParams()
   const [ posts, errorPosts ] = usePostsByTag(searhParams.getAll("tag"))
   
   if(errorPosts){
@@ -16,7 +16,7 @@ const Home = () => {
     <div>
       <Header />
       <div className='w-full flex flex-col items-center pt-12'>
-          <h1 style={{ fontFamily: "'EB Garamond', serif" }} className='text-5xl font-garamond text-center'>Welcome to The Learning Experience</h1>
+          <h1 className='text-5xl text-center font-family-garamond'>Welcome to The Learning Experience</h1>
           <div className='grid grid-cols-1 lg:grid-cols-2 gap-1 w-10/12 max-w-4xl pt-12'>
           {posts ? posts.map((post:Post, index:number) => {
             return (
