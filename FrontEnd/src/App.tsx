7980a35c2e50a7aa0e4f2d8991df3d551e06e81b
LucasNarZ <LucasNarloch123@gmail.com>
2025-06-19 16:42:50 -0300

    new create and edit routes and change from id to slug

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -4,18 +4,20 @@ import Post from './pages/post'
 import SignIn from './pages/signIn'
 import SignUp from './pages/signUp'
 import Profile from './pages/profile'
-import PostEditor from './pages/editor'
+import CreatePost from './pages/createPost'
+import EditPost from './pages/editPost'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/post/:postId" element={<Post />} />
+        <Route path="/post/:slug" element={<Post />} />
         <Route path='/signIn' element={<SignIn />}/>
         <Route path='/signUp' element={<SignUp />}/>
         <Route path='/profile' element={<Profile />}/>
-        <Route path='/editor' element={<PostEditor />}/>
+        <Route path='/createPost' element={<CreatePost />}/>
+        <Route path='/editPost/:postId' element={<EditPost />}/>
       </Routes>
     </BrowserRouter>
   )
