262b034576c432dbb4724927a8abc08e5c14625b
LucasNarZ <lucasnarloch123@gmail.com>
2025-06-24 13:05:37 -0300

    new tokens

--- BackEnd/src/constants.ts
+++ BackEnd/src/constants.ts
@@ -1,5 +1,7 @@
 export const postsRepositoryToken = 'POSTS_REPOSITORY'
 export const usersRepositoryToken = 'USERS_REPOSITORY'
+export const followsRepositoryToken = 'FOLLOW_REPOSITORY'
+export const sequelizeToken = "SEQUELIZE_TOKEN"
 export const jwtConstants = {
     secret:process.env.JWT_SECRET
 }
