304e25539dcec6b6fee3b79396cf1a9de3c6632a
LucasNarZ <lucasnarloch123@gmail.com>
2025-06-24 13:05:04 -0300

    follow routes and services

--- BackEnd/src/User/users.controller.ts
+++ BackEnd/src/User/users.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Body, Req, Put, UseGuards, UnauthorizedException, Param } from "@nestjs/common";
-import { AuthGuard } from "src/auth/auth.guard";
+import { Controller, Get, Body, Req, Put, UseGuards, UnauthorizedException, Param, Post, BadRequestException } from "@nestjs/common";
+import { AuthGuard } from "src/auth/guards/auth.guard";
 import { UsersService } from "./users.service";
 import { Request } from "express";
-import { updateUserDto } from "src/dtos/updateUser.dto";
+import { updateUserDto } from "src/User/dtos/updateUser.dto";
 import { UserNotFoundException } from "src/exceptions/UserNotFound.exception";
 import { AuthRequest } from "src/interfaces/authRequest.interface";
+import { FollowUserDto } from "./dtos/followUser.dto";
+import { isUUID } from "class-validator";
 
 @Controller("user")
 export class UsersController {
@@ -14,42 +16,67 @@ export class UsersController {
 
     @Get("basic/:userId")
     async findUserBasicInfo(@Param("userId") userId:string) {
-        return await this.usersService.findUserBasic(userId)
+        return await this.usersService.findUserBasic(userId);
     }
 
 
     @UseGuards(AuthGuard)
     @Get("profile")
     async findUser(@Req() req:AuthRequest) {
-        const id = req.user.userId
+        const id = req.user.userId;
         if(!id){
-            throw new UnauthorizedException("User is not logged")
+            throw new UnauthorizedException("User is not logged");
         }
-        const user = await this.usersService.findUser(id)
+        const user = await this.usersService.findUser(id);
         if(!user){
-            throw new UserNotFoundException(`user with id ${id} not found`)
+            throw new UserNotFoundException(`user with id ${id} not found`);
         }
-        return user
+        return user;
     }
 
     @Get(":userId")
     async findUserPublic(@Param("userId") userId:string){
-        return await this.usersService.findUserPublic(userId)
+        return await this.usersService.findUserPublic(userId);
     }
 
     @Get(":id/posts")
     async getUserPosts(@Req() req:Request) {
         const { id } = req.params;
-        const posts = await this.usersService.getPostByUser(id)
-        return posts
+        const posts = await this.usersService.getPostByUser(id);
+        return posts;
     }
 
     @UseGuards(AuthGuard)
     @Put("update")
     async updateUser(@Req() req:AuthRequest, @Body() body:updateUserDto) {
-        const id = req?.user?.userId
+        const id = req?.user?.userId;
 
-        return await this.usersService.updateUser(id, body)
+        return await this.usersService.updateUser(id, body);
+    }
+
+    @UseGuards(AuthGuard)
+    @Post("/follow")
+    async followUser(@Req() req:AuthRequest, @Body() body:FollowUserDto){
+        const followerId = req?.user?.userId;
+        return this.usersService.followUser(followerId, body.followedId);
+    }
+
+    @UseGuards(AuthGuard)
+    @Post("/unfollow")
+    async unfollowUser(@Req() req:AuthRequest, @Body() body:FollowUserDto){
+        const followerId = req?.user?.userId;
+        return this.usersService.unfollowUser(followerId, body.followedId);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get("/follow/:followedId")
+    async getFollowUser(@Req() req:AuthRequest, @Param("followedId") followedId:string){
+        const followerId = req?.user?.userId;
+        if(!isUUID(followedId)){
+            throw new BadRequestException("The followedId must be an uuid.")
+        }
+
+        return await this.usersService.getFollowUser(followerId, followedId);
     }
 
 }
--- BackEnd/src/User/users.service.ts
+++ BackEnd/src/User/users.service.ts
@@ -1,17 +1,23 @@
-import { Inject, Injectable, NotFoundException } from "@nestjs/common";
-import { postsRepositoryToken, usersRepositoryToken } from "src/constants";
+import { BadRequestException, ConflictException, Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { followsRepositoryToken, postsRepositoryToken, sequelizeToken, usersRepositoryToken } from "src/constants";
 import { User } from "./user.entity";
-import { createUserDto } from "src/dtos/user.dto";
+import { createUserDto } from "src/User/dtos/user.dto";
 import { Post } from "src/Post/post.entity";
-import { updateUserDto } from "src/dtos/updateUser.dto";
+import { updateUserDto } from "src/User/dtos/updateUser.dto";
+import { Follow } from "./follow.entity";
+import { Sequelize } from "sequelize";
 
 @Injectable()
 export class UsersService {
     constructor(
+        @Inject(sequelizeToken)
+        private readonly sequelize: Sequelize,
         @Inject(usersRepositoryToken)
         private usersRepository: typeof User,
         @Inject(postsRepositoryToken)
-        private postsRepository: typeof Post
+        private postsRepository: typeof Post,
+        @Inject(followsRepositoryToken)
+        private followsRepository: typeof Follow
     ) {}
 
     async createUser({name, email, password}:createUserDto) {
@@ -41,7 +47,7 @@ export class UsersService {
 
     async findUserPublic(id:string) {
         const user = await this.usersRepository.findOne({
-            attributes:["name", "profileImgUrl", "bio", "followers"],
+            attributes:["name", "profileImgUrl", "bio", "followers", "createdAt"],
             where:{
                 id
             }
@@ -73,6 +79,93 @@ export class UsersService {
                 }
             }
         )
+    }
+
+    async followUser(followerId:string, followedId:string){
+        if(followedId === followerId){
+            throw new BadRequestException("You can't follow yourself.")
+        }
+        return await this.sequelize.transaction(async (t) => {
+            const relation = await this.followsRepository.findOne({
+                where:{
+                    followerId,
+                    followedId,
+                    lock: t.LOCK.UPDATE
+                },
+                transaction: t
+            })
+
+            if(relation){
+                throw new ConflictException("You already follow this user.")
+            }
+
+            await this.usersRepository.increment(1, {
+                where:{
+                    userId: followedId
+                },
+                transaction: t
+            })
+
+            await this.followsRepository.create(
+                {
+                    followerId,
+                    followedId
+                },
+                {transaction: t}
+            )
+
+            return { message: 'Followed successfully' };
+        })
+
+    }
+
+    async unfollowUser(followerId:string, followedId:string){
+        if(followedId === followerId){
+            throw new BadRequestException("You can't unfollow yourself.")
+        }
+        return await this.sequelize.transaction(async (t) => {
+            const relation = await this.followsRepository.findOne({
+                where:{
+                    followerId,
+                    followedId,
+                    lock: t.LOCK.UPDATE
+                },
+                transaction: t
+            })
+
+            if(!relation){
+                throw new NotFoundException("You don't follow this user.")
+            }
+
+            await this.usersRepository.decrement(1, {
+                where:{
+                    userId: followedId
+                },
+                transaction: t
+            })
+
+            await this.followsRepository.destroy(
+                {
+                    where:{
+                        followerId,
+                        followedId
+                    },
+                    transaction: t
+                }
+            )
+
+            return { message: 'Unfollowed successfully' };
+        })
+
+    }
+
+    async getFollowUser(followerId:string, followedId:string) {
+        return !!(await this.followsRepository.findOne({
+            where:{
+                followerId,
+                followedId
+            }
+        }))
     }
 
 }
