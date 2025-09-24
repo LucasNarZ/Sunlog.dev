e8bddd71dd6fae963718a65954a5bdc979d7b08b
LucasNarZ <LucasNarloch123@gmail.com>
2025-09-24 08:07:37 -0300

    add loading skeleton

--- frontend/src/lib/fetchUser.ts
+++ frontend/src/lib/fetchUser.ts
@@ -3,9 +3,7 @@ import type { User } from "@/types/user";
 
 export const fetchUser = async (): Promise<User | null> => {
   try {
-    const response = await apiClient.get(`/user/profile`, {
-      withCredentials: true,
-    });
+    const response = await apiClient.get("/user/profile", {withCredentials: true}) 
     return response.data;
   } catch (err) {
     console.error(err);
