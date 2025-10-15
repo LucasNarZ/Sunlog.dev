5e2e4e871244f10baed73eb8d5fbad7bfd32a055
LucasNarZ <lucasnarloch123@gmail.com>
2025-10-15 08:38:00 -0300

    fix: deploy

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const isServer = typeof window === "undefined";
-
 export const apiClient = axios.create({
-  baseURL: isServer ? "http://nginx/api" : "/api",
+  baseURL: "/api",
 });
