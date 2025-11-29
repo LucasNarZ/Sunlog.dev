f77a80ee472fd30cccb70481159d3380db452199
LucasNarZ <LucasNarloch123@gmail.com>
2025-11-29 12:52:36 -0300

    feat: comments

--- frontend/src/app/devlog/[slug]/page.tsx
+++ frontend/src/app/devlog/[slug]/page.tsx
@@ -9,69 +9,71 @@ import { getPost } from "@/lib/getPost";
 import { getFollow } from "@/lib/getFollow";
 import PostInteractions from "@/components/PostInteractions";
 import { Metadata } from "next";
+import CommentsSection from "@/components/commentSection";
 
 export async function generateMetadata({
-  params,
+    params,
 }: {
-  params: Promise<{ slug: string }>;
+    params: Promise<{ slug: string }>;
 }): Promise<Metadata> {
-  const { slug } = await params;
-  const post = await getPost(slug);
+    const { slug } = await params;
+    const post = await getPost(slug);
 
-  return {
-    title: post.title,
-    description: post.description,
-  };
+    return {
+        title: post.title,
+        description: post.description,
+    };
 }
 
 const Post = async ({ params }: { params: Promise<{ slug: string }> }) => {
-  const { slug } = await params;
-  const post = await getPost(slug);
-  if (!post) {
-    redirect("/post-not-found");
-  }
-  const user = await getAuthor(post?.userId);
-  const following = await getFollow(user.id);
+    const { slug } = await params;
+    const post = await getPost(slug);
+    if (!post) {
+        redirect("/post-not-found");
+    }
+    const user = await getAuthor(post?.userId);
+    const following = await getFollow(user.id);
 
-  return (
-    <div className="min-h-screen w-full bg-white text-gray-900 flex flex-col items-center">
-      <Header />
-      {post ? (
-        <article className="max-w-3xl w-full px-6 sm:px-10 pb-20 mt-20 break-words">
-          {post.previewImgUrl && (
-            <Image
-              src={post.previewImgUrl}
-              alt="Post preview"
-              className="w-full h-72 object-cover rounded-xl mb-8 shadow-md"
-              width={0}
-              height={0}
-              unoptimized
-            />
-          )}
-          <h1 className="text-4xl font-extrabold mb-4 leading-tight font-family-garamond">
-            {post.title}
-          </h1>
-          <PostInteractions
-            user={user}
-            post={post}
-            initialFollowing={following}
-          />
-          <div className="prose prose-lg max-w-none leading-relaxed font-serif text-gray-800">
-            <ReactMarkdown
-              remarkPlugins={[remarkGfm]}
-              rehypePlugins={[rehypeRaw]}
-            >
-              {post.content}
-            </ReactMarkdown>
-          </div>
-        </article>
-      ) : (
-        <div className="mt-40 text-xl font-medium text-gray-600">
-          Loading...
+    return (
+        <div className="min-h-screen w-full bg-white text-gray-900 flex flex-col items-center">
+            <Header />
+            {post ? (
+                <article className="max-w-3xl w-full px-6 sm:px-10 pb-20 mt-20 break-words">
+                    {post.previewImgUrl && (
+                        <Image
+                            src={post.previewImgUrl}
+                            alt="Post preview"
+                            className="w-full h-72 object-cover rounded-xl mb-8 shadow-md"
+                            width={0}
+                            height={0}
+                            unoptimized
+                        />
+                    )}
+                    <h1 className="text-4xl font-extrabold mb-4 leading-tight font-family-garamond">
+                        {post.title}
+                    </h1>
+                    <PostInteractions
+                        user={user}
+                        post={post}
+                        initialFollowing={following}
+                    />
+                    <div className="prose prose-lg max-w-none leading-relaxed font-serif text-gray-800">
+                        <ReactMarkdown
+                            remarkPlugins={[remarkGfm]}
+                            rehypePlugins={[rehypeRaw]}
+                        >
+                            {post.content}
+                        </ReactMarkdown>
+                    </div>
+                </article>
+            ) : (
+                <div className="mt-40 text-xl font-medium text-gray-600">
+                    Loading...
+                </div>
+            )}
+            <CommentsSection postId={post.id} />
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Post;
