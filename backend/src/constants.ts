a462c3812de11bafc530461a6549655498fb35c9
LucasNarZ <LucasNarloch123@gmail.com>
2025-11-29 12:52:02 -0300

    feat: comments repository token

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -2,6 +2,7 @@ export const postsRepositoryToken = 'POSTS_REPOSITORY';
 export const usersRepositoryToken = 'USERS_REPOSITORY';
 export const followsRepositoryToken = 'FOLLOW_REPOSITORY';
 export const likesRepositoryToken = 'LIKES_REPOSITORY';
+export const commentRepositoryToken = 'COMMENT_REPOSITORY';
 export const postStatusesRepositryToken = 'POSTSTATUSES_REPOSITORY';
 export const sequelizeToken = 'SEQUELIZE_TOKEN';
 export const jwtConstants = {
