659dd2c5f1d8b3b62d85d9823af14656e21a92e5
LucasNarZ <lucasnarloch123@gmail.com>
2026-01-21 16:57:19 -0300

    fix: deploy script order

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,10 @@ cd ~/Sunlog.dev
 git fetch origin
 git reset --hard origin/main
 
-sudo docker compose -f docker-compose.prod.yml down
-
-npm run install-all
-
 docker compose -f docker-compose.prod.yml pull
+
+docker compose -f docker-compose.prod.yml down
+
 docker compose -f docker-compose.prod.yml up -d
 
 cd backend
