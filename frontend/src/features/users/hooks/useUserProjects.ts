7e650efff8de5f26622b71405a7b4fedad81611b
lucas <lucasnarloch123@gmail.com>
2026-04-14 19:33:39 -0300

    fix: use correct get user projects endpoint

--- frontend/src/features/users/hooks/useUserProjects.ts
+++ frontend/src/features/users/hooks/useUserProjects.ts
@@ -2,17 +2,14 @@ import { useEffect, useState } from 'react';
 import { fetchUserProjects } from '../services/fetchUserProjects';
 import { Project } from '@/features/projects/types/project';
 
-const useUserProjects = (
-	userId: string | undefined,
-): [Project[] | null, unknown, boolean] => {
+const useUserProjects = (): [Project[] | null, unknown, boolean] => {
 	const [error, setError] = useState<unknown>(null);
 	const [response, setResponse] = useState<Project[] | null>(null);
 	const [loading, setLoading] = useState<boolean>(true);
 	useEffect(() => {
 		(async () => {
-			if (!userId) return;
 			try {
-				const data = await fetchUserProjects(userId);
+				const data = await fetchUserProjects();
 				setResponse(data);
 			} catch (err) {
 				setError(err);
@@ -20,7 +17,7 @@ const useUserProjects = (
 				setLoading(false);
 			}
 		})();
-	}, [userId]);
+	}, []);
 
 	return [response, error, loading];
 };
